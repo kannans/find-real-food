fff0d3598bb57a0dc1792ba78c5be834d2171cc4
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-10-18 19:31:10 +0530

    imagemagick installed

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -143,6 +143,75 @@ $(window).resize(function(){
 	$('.caption_part').css('top',porthalf+'px');
 })*/
 
+$(window).load(function(e) {
+	
+	$('.light_pop_trigger').click(function(e){
+		var current=$(this);
+		e.preventDefault();
+		$('.pop_close').fadeOut(500);
+			$('.light_popup').fadeOut(500);
+			$('.light_pop_overlay').remove();
+		var popid=$(this).attr('href');
+		$('body').append('<div class="light_pop_overlay"></div>');
+		$('.light_pop_overlay').fadeIn(500);
+		$('.pop_close').fadeIn(50);
+		$('.pop_close').show();
+		$(popid).fadeIn(500);	
+		$("#user_signup_form").find("input[type=text], textarea").val("");
+		$("#user_signup_form").find("select").val("").removeAttr('selected');
+		$("label.error").hide();
+		//$('.light_pop_container').height($(popid).height()-15)	
+		//$('.light_pop_body').height($(popid).height()-30);
+		
+		if(current.attr('data-iframe'))
+		{
+			var popframe=current.attr('data-iframe');
+			$(popid+' .light_pop_body').html('<iframe border="0" src="'+popframe+'"></iframe> ');
+		}
+		else
+		{
+			return false;
+		}
+	})
+
+	
+	$('.pop_close').click(function(){
+			$('.pop_close').fadeOut(500);
+			$('.light_popup').fadeOut(500);
+			$('.light_pop_overlay').remove();
+		})
+		$(document).on('click','.light_pop_overlay',(function(){
+			$('.pop_close').fadeOut(500);
+			$('.light_popup').fadeOut(500);
+			$('.light_pop_overlay').remove();
+		}))
+		
+    
+});
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        $('.pop_close').fadeOut(500);
+			$('.light_popup').fadeOut(500);
+			$('.light_pop_overlay').remove();
+    }
+};
+	$(window).resize(function(){
+		//$('.light_pop_container').height($('.light_popup').height()-15)	
+		//$('.light_pop_body').height($('.light_popup').height()-30);
+		})
+		
+		
+
+
+	$(window).resize(function(){
+		//$('.light_pop_container').height($('.light_popup').height()-15)	
+		//$('.light_pop_body').height($('.light_popup').height()-30);
+		})
+		
+		
+
 
 
 
