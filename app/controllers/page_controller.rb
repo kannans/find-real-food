e77d69d079b509ef79ada9abe33b39f8463cd1c7
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-10-20 20:19:56 +0530

    Issue fixed

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -5,12 +5,12 @@ class PageController < ApplicationController
 
   def contact
   	if params[:sbmt_con] !=''
-  	@contact.fname = params[:txt_con_lname]
-  	@contact.lname = params[:txt_con_lname]
-  	@contact.email = params[:txt_con_email]
-  	@contact.phone = params[:txt_con_ph]
-  	@contact.comment = params[:txtar_con_comments]
-  	ContactMailer.contact_mail(@contact).deliver
+  	#@contact.fname = params[:txt_con_lname]
+  	#@contact.lname = params[:txt_con_lname]
+  	#@contact.email = params[:txt_con_email]
+  	#@contact.phone = params[:txt_con_ph]
+  	#@contact.comment = params[:txtar_con_comments]
+  	ContactMailer.contact_mail(params).deliver
   	flash[:notice] = "Your Details have been Submited successfully, Administrator will contact you shortly"
   	redirect_to "/contact-us"
   	end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
   default from: "rmarktest@gmail.com"
   def contact_mail(contact)
-    @contact = contact
+    @contact = contact[:txt_con_lname]
     mail(to: 'rmarktest1@gmail.com', subject: 'Find Real Food - Contact Us')
   end
 end
