40999a65fb9c5d6ff82417e2fe7c8e6a60e0106a
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-10-20 21:00:38 +0530

    remove error

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -4,13 +4,13 @@ class PageController < ApplicationController
   end
 
   def contact
-  	if params[:sbmt_con] !=''
+  	if params[:sbmt_con] =='Submit'
   	#@contact.fname = params[:txt_con_lname]
   	#@contact.lname = params[:txt_con_lname]
   	#@contact.email = params[:txt_con_email]
   	#@contact.phone = params[:txt_con_ph]
   	#@contact.comment = params[:txtar_con_comments]
-  	ContactMailer.contact_mail(params).deliver
+  	ContactMailer.contact_mail(params[:txt_con_lname]).deliver
   	flash[:notice] = "Your Details have been Submited successfully, Administrator will contact you shortly"
   	redirect_to "/contact-us"
   	end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
   default from: "rmarktest@gmail.com"
   def contact_mail(contact)
-    @contact = contact[:txt_con_lname]
+    @contact = contact
     mail(to: 'rmarktest1@gmail.com', subject: 'Find Real Food - Contact Us')
   end
 end
