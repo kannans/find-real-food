890c9ae66c7c84c786c247ea4599b0be3208a875
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-10-27 19:49:57 +0530

    client changes

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -4,11 +4,11 @@
   end
 
   def contact
-  	#if params[:sbmt_con] =='Submit'
+  	if params[:sbmt_con] =='Submit'
   	
-  	ContactMailer.contact_mail(params).deliver
+  	#ContactMailer.contact_mail(params).deliver
   	flash[:notice] = "Your Details have been Submited successfully, Administrator will contact you shortly"
   	redirect_to "/contact-us"
-  	#end
+  	end
   end
 end
