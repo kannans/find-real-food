1f46366d5e8bb71dd60b803362dc049d9ad5a720
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-10-27 19:44:10 +0530

    client changes

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,14 +1,14 @@
-class PageController < ApplicationController
+  class PageController < ApplicationController
   def index
   	@page = Page.find(params[:slug])
   end
 
   def contact
-  	if params[:sbmt_con] =='Submit'
+  	#if params[:sbmt_con] =='Submit'
   	
   	ContactMailer.contact_mail(params).deliver
   	flash[:notice] = "Your Details have been Submited successfully, Administrator will contact you shortly"
   	redirect_to "/contact-us"
-  	end
+  	#end
   end
 end
