03d2beb59e0dbea034c28107ad799843491b4624
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-10-27 20:04:52 +0530

    client changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ RealFood::Application.configure do
   authentication: 'plain',
   enable_starttls_auto: true }
 
-  config.action_mailer.default_url_options = { :host => '72.167.38.159' }  
+  
 
 
 
