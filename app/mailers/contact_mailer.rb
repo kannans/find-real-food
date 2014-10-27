d152d1147526a29f1d4662f5a62c3ce769da72a2
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-10-27 20:20:33 +0530

    client changes

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ActionMailer::Base
   default from: "rmarktest@gmail.com"
   def contact_mail(contact)
     #@contact = contact
-    #mail(to: 'rmarktest1@gmail.com', subject: 'Find Real Food - Contact Us')
+    mail(to: 'rmarktest1@gmail.com', subject: 'Find Real Food - Contact Us')
   end
 end
