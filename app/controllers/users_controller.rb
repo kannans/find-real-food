47cef75fd8545480af479696d7e37c0fbbcb51e9
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-10-31 18:37:40 +0530

    add password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < Devise::RegistrationsController
 
     if credit_card.valid?
       # or gateway.purchase to do both authorize and capture
-      response = gateway.authorize(1000, credit_card, :ip => "72.167.38.159")
+      response = gateway.authorize(1000, credit_card, :ip => "127.0.0.1")
       if response.success?
         gateway.capture(1000, response.authorization)
         
