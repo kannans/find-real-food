6bcee806aad0d5beff4195474181b2bc99251def
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-10-31 20:33:50 +0530

    forgot password

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -2,7 +2,8 @@ class FaqController < ApplicationController
   def index
   	@faqs = Faq.all
 
- 
+  @user = User.find_by_email('rmarktest@gmail.com')
+  ContactMailer.forgot_mail(@user).deliver
 
   end
 end
