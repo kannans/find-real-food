3314e5582a989e9299ad5fd9fb5b523df4a96c71
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-11-03 17:51:40 +0530

    brands related to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,7 +25,7 @@ class CategoriesController < ApplicationController
 	    if @location !=''
 		  @products = Product.sort_by_rating(@location,search,@category)
 		  @products_locations = Product.sort_by_rating(@location,search,@category).collect{|c| c.location_id}.join(',')
-		  @brand_ids = @products.collect{|c| c.brand_id}.join(',')
+		  @brand_ids = Product.sort_by_rating(@location,search,@category).collect{|c| c.brand_id}.join(',')
 		  @brands = Brand.where("id in (#{@brand_ids})")
 		  if @products_locations!=''
 	      @locations = Location.where("id in (#{@products_locations})")
