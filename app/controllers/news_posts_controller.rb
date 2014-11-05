7f8411f48ac9281dbd77ceadd2aad9ad5732bef9
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-11-05 22:38:24 +0530

    styles updated

--- app/controllers/news_posts_controller.rb
+++ app/controllers/news_posts_controller.rb
@@ -1,11 +1,11 @@
 class NewsPostsController < ApplicationController
   def index
-  	@newsposts = NewsPost.paginate(page: params[:page], per_page: 3).order('title DESC')
+  	@newsposts = NewsPost.paginate(page: params[:page], per_page: 3).order('created_at DESC')
   end
   def more_details
 		
 		@newspost = NewsPost.find(params[:slug])
-		@newsposts = NewsPost.order('title DESC').last(10)
+		@newsposts = NewsPost.order('created_at DESC').last(10)
 		 
 	end
 end
