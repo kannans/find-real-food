5ae7a725776bc90972ef9f4df60d397364643c4d
Ihor <izinko@gmail.com>
2014-11-07 15:46:19 +0200

    fix avatar, cover_photo validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,10 +23,12 @@ class User < ActiveRecord::Base
   has_one  :promo_code
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :baner => "800x300" }, :default_url => "/avatars/:style/missing.png"
-  validates_attachment :avatar, :presence => false, :content_type => {:content_type => /\Aimage\/.*\Z/}
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  # validates_attachment :avatar, :presence => false, :content_type => {:content_type => /\Aimage\/.*\Z/}
 
   has_attached_file :cover_photo, :styles => { :medium => "300x300>", :thumb => "100x100>", :baner => "800x300" }, :default_url => "/avatars/:style/missing.png"
-  validates_attachment :cover_photo, :presence => false, :content_type => {:content_type => /\Aimage\/.*\Z/}
+  validates_attachment_content_type :cover_photo, :content_type => /\Aimage\/.*\Z/
+  # validates_attachment :cover_photo, :presence => false, :content_type => {:content_type => /\Aimage\/.*\Z/}
 
 	acts_as_api
 
