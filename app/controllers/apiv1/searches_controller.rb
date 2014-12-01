18187aab44a754b7f93a7dbc00a593d5724b75ae
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-01 15:52:59 +0530

    search webservice

--- app/controllers/apiv1/searches_controller.rb
+++ app/controllers/apiv1/searches_controller.rb
@@ -87,9 +87,6 @@ class Apiv1::SearchesController < Api::BaseController
 
     search = Search.new({
       :brands => @resources[:brands].nil? ? nil : @resources[:brands].paginate(:per_page => search_result_limit, :page => params[:page]),
-      :categories => @resources[:categories].nil? ? nil : @resources[:categories].paginate(:per_page => search_result_limit, :page => params[:page]),
-      :locations => @resources[:locations].nil? ? nil : @resources[:locations].paginate(:per_page => search_result_limit, :page => params[:page]),
-      :products => @resources[:products].nil? ? nil : @resources[:products].paginate(:per_page => search_result_limit, :page => params[:page]),
       
     })
 
