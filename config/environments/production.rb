85d50977bcfdef163039763675c52283a489cacf
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-04 20:39:31 +0530

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ config.action_mailer.sendmail_settings = {
   address: 'smtp.gmail.com',
   port: 587,
   domain: 'gmail.com',
-  user_name: "findrealfoodapp@gmail.com",
-  password: "WAPF2014",
+  user_name: "rmarktest@gmail.com",
+  password: "Testing12",
   authentication: 'plain',
   enable_starttls_auto: true }
 
