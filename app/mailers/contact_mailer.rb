babfd5ef20f2b4688e5b8e52af202896ade2a030
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-04 20:35:41 +0530

    mail

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default from: "findrealfoodapp@gmail.com"
+  default from: "rmarktest@gmail.com"
   def contact_mail(contact)
     @contact = contact
     mail(to: 'rmarktest1@gmail.com', subject: 'Find Real Food - Contact Us')
