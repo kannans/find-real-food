60acdd4099865ce066123c8332b8987db7ab6f0a
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-09 12:24:07 +0530

    search

--- config/routes.rb
+++ config/routes.rb
@@ -106,6 +106,8 @@ RealFood::Application.routes.draw do
 
     resources :products, :only => [:show, :create]
 
+    resources :sessions, :only => [:create]
+
     post ':ratable_type/:ratable_id/ratings', :to => 'ratings#create'
     post ':flaggable_type/:flaggable_id/flags', :to => 'flag_requests#create'
 
