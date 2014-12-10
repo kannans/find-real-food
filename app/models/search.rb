f3febb5d429a196cf813c444f31c9e476041c3f9
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-10 17:22:01 +0530

    service

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,17 +1,10 @@
 class Search < ActiveRecord::Base
-  set_table_name :news_posts #temporary fix. need to figure out how to get this model to just include ActiveModel
+  #self.table_name = 'news_posts' #temporary fix. need to figure out how to get this model to just include ActiveModel
 
 
   attr_accessor :products, :brands, :categories, :users, :locations, :pages
 
   acts_as_api
 
-  api_accessible :search do |template|
-    template.add :products, :template => :product
-    template.add :brands, :template => :brand
-    template.add :categories, :template => :category
-    template.add :users, :template => :user
-    template.add :locations, :template => :location
-    template.add :pages
-  end
+   
 end
