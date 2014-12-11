0fa3ee50989d9c69da30ccf8ac3b68a4b819a44c
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-11 14:30:08 +0530

    update location

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -1,10 +1,33 @@
 class FaqController < ApplicationController
   def index
   	@faqs = Faq.all
-
+  	
   end
 
   def updatedlocations
   	@records = Location.where("latitude !=''")
+
+  	locations = Location.where("address !=''")
+       state = ''
+       locations.each do |l|
+         location = Location.where(:id => l.id).first
+         if location.zipd
+          zipcode = location.zip
+          connection = ActiveRecord::Base.connection()
+          results = connection.execute("select STATE from master_zipcode where ZIP_CODE='#{zipcode}' limit 1")
+          results.each do |row|
+            state =  row[0]
+          end
+          location.update_attributes({
+            :address => location.address,
+            :zip => location.zip,
+            :city => location.city,
+            :state => state
+           })
+         end
+         
+       end
+
+       
   end
 end
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,8 +3,9 @@ Geocoder.configure(
   lookup: :google,
 
   # geocoding service request timeout (in seconds)
-  timeout: 30,
+  timeout: 3,
 
+  api_key:  ['PhPKap7tVG3jpQr2xZq-t0rSTs8=', 'gme-realfoodmobilellc'],
   # default units
   units: :km
 )
