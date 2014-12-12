e1269d2341b847f5dea1653451155dd1d7263704
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-12 20:23:12 +0530

    webservice

--- app/controllers/apiv1/searches_controller.rb
+++ app/controllers/apiv1/searches_controller.rb
@@ -6,20 +6,7 @@ class Apiv1::SearchesController < Apiv1::BaseController
 
   api :GET, '/search', "Perform a multi-object search"
    
-  desc <<-EOT
-    Params are passed in the url via ?q[]
-
-    ?q[ brand ]\n
-    ?q[ filter ]\n
-    ?q[ quality_rating_name_eq ]
-
-    ?q[ category_id_eq ]
-      Searches by a specific category id. If present also limits the search to just products.
-
-    To sort products:
-    sort=quality
-    sort=rating
-  EOT
+  
 
   def search
     
