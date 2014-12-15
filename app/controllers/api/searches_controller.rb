1f94fdf3d818f2803ed974d51dc6492848aaac23
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-15 20:13:36 +0530

    api files

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -68,12 +68,7 @@ class Api::SearchesController < Api::BaseController
     @resources[:users] = User.search(q).result  if f.nil? || f == "User"
 
 
-    if q[:category_id_eq]
-      q[:products_category_id_eq] = q[:category_id_eq]
-      @resources[:brands] = Brand.approved.joins(:products).search(q).result.group("brands.name")
-      @resources[:categories] = nil
-      @resources[:users] = nil
-    end
+   
 
     search = Search.new({
       :brands => @resources[:brands].nil? ? nil : @resources[:brands].paginate(:per_page => search_result_limit, :page => params[:page]) 
