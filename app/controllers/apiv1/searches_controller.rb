dcb7d3969db9d4246b517a5b6882fb7d23bec1b9
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-15 17:22:21 +0530

    webservice

--- app/controllers/apiv1/searches_controller.rb
+++ app/controllers/apiv1/searches_controller.rb
@@ -25,63 +25,10 @@ class Apiv1::SearchesController < Apiv1::BaseController
   EOT
 
   def search
-    search_result_limit = 20
-
-    q = params[:q]
-
-    unless q[:name_cont].nil?
-      q[:name_cont_all] = q[:name_cont].split(' ')
-      q[:title_cont_all] = q[:name_cont_all]
-      q.delete(:name_cont)
-    end
-
-    f = q[:filter]
-
-    unless params[:sub_filter].nil?
-      q[:order_by_phone_eq] = true if params[:sub_filter] == 'order_by_phone'
-      q[:brand_order_by_phone_eq] = true if params[:sub_filter] == 'order_by_phone'
-
-      q[:order_by_online_or_third_party_available_eq] = true if params[:sub_filter] == 'order_by_online'
-      q[:brand_order_by_online_or_brand_third_party_available_eq] = true if params[:sub_filter] == 'order_by_online'
-
-      q[:store_farmers_market_eq] = true if params[:sub_filter] == 'store_or_farmers_market'
-      q[:brand_store_farmers_market_eq] = true if params[:sub_filter] == 'store_or_farmers_market'
-
-    end
-
-    @resources = {}
-
-    @resources[:brands] = Brand.approved.search(q).result if f.nil? || f == "Brand"
-    @resources[:categories] = Category.search(q).result  if f.nil? || f == "Category"
-    @resources[:locations] = Location.search(q).result  if f.nil? || f == "Location"
-
-    if f.nil? || f == "Product"
-      if params[:sort] == "rating"
-        @resources[:products] = Product.search_and_sort_by_rating(q).result
-      elsif params[:sort] == "quality"
-        @resources[:products] = Product.search_and_sort_by_quality(q).result
-      else
-        @resources[:products] = Product.search(q).result
-      end
-    end
-
-    @resources[:users] = User.search(q).result  if f.nil? || f == "User"
-
-
-    if q[:category_id_eq]
-      q[:products_category_id_eq] = q[:category_id_eq]
-      @resources[:brands] = Brand.approved.joins(:products).search(q).result.group("brands.name")
-      @resources[:categories] = nil
-      @resources[:users] = nil
-    end
-
-    search = Search.new({
-      :brands => @resources[:brands].nil? ? nil : @resources[:brands].paginate(:per_page => search_result_limit, :page => params[:page]),
-   
-    })
+     
 
     respond_to do |format|
-      format.json { render_for_api :search, :json => search, :meta => { :success => true} }
+      format.json { render_for_api :search, :json => "text", :meta => { :success => true} }
     end
 
   end
