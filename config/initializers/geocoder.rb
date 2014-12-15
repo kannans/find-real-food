2b158bc3791422e9f1997be0362e1babd1272482
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-15 17:38:10 +0530

    webservice

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -4,7 +4,7 @@ Geocoder.configure(
   :lookup => :google_premier,
   # to use an API key:
   
-  :api_key =>  ['PhPKap7tVG3jpQr2xZq-t0rSTs8=', 'gme-realfoodmobilellc'],
+  :api_key =>  ['PhPKap7tVG3jpQr2xZq-t0rSTs8=', 'gme-realfoodmobilellc', "google"],
 
   # geocoding service request timeout, in seconds (default 3):
   :timeout => 5,
