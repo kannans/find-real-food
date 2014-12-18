edb391d18293305e69d944b8d7ea95a23942e50f
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-18 22:03:53 +0530

    facebook login fix

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -15,13 +15,21 @@ class Api::SessionsController < Devise::SessionsController
     if params[:facebook_id]
       resource = User.where(:email => params[:email], :facebook_id => params[:facebook_id]).first
       if resource.nil?
-        resource = User.create!(:email => params[:email], :facebook_id => params[:facebook_id], :password => "empty1234")
-        params[:user].delete(:id)
-        params[:user].delete(:created_at)
-        params[:user].delete(:active_subscription)
-        params[:user][:avatar] = open(params[:user][:avatar])
-        resource.update_attributes!(params[:user])
-        resource.reload
+        resource = User.where(:email => params[:email]).first
+        if resource.nil?
+          resource = User.create!(:email => params[:email], :facebook_id => params[:facebook_id], :password => "empty1234")
+          params[:user].delete(:id)
+          params[:user].delete(:created_at)
+          params[:user].delete(:active_subscription)
+          params[:user][:avatar] = open(params[:user][:avatar])
+          resource.update_attributes!(params[:user])
+          resource.reload
+        else
+          newarr = Array.new
+          newarr[0] = params[:facebook_id]
+          resource.update_attributes!(newarr)
+          
+        end
       end
     else
       resource = User.find_for_database_authentication(:email => params[:email])
