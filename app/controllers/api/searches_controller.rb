8743f949f767da317d8f225d8619818402a7afd1
suhasg <suhasg87@gmail.com>
2014-12-19 12:05:21 +0530

    Fix for search result format

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -94,7 +94,7 @@ class Api::SearchesController < Api::BaseController
       @resources[:users] = nil
     end
 
-    @search = {:search => {
+    @search = Search.new({
       :brands => @resources[:brands].nil? ? nil : @resources[:brands].paginate(:per_page => search_result_limit, :page => params[:page]) ,
       :categories => @resources[:categories].nil? ? nil : @resources[:categories].paginate(:per_page => search_result_limit, :page => params[:page]) ,
       :locations => @resources[:locations].nil? ? nil : @resources[:locations].paginate(:per_page => search_result_limit, :page => params[:page]) ,
@@ -107,10 +107,10 @@ class Api::SearchesController < Api::BaseController
           :products => @resources[:products].nil? ? 0 : (@resources[:products].length / search_result_limit.to_f).ceil,
           :users => @resources[:users].nil? ? 0 : (@resources[:users].length / search_result_limit.to_f).ceil
       }
-    }}
+    })
 
     respond_to do |format|
-      format.json {render json: @search.merge(:success => true)}
+      format.json { render_for_api :search, :json => @search, :meta => { :success => true} }
     end
   end
 
