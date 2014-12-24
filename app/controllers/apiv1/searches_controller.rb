90b9c4b9089af1458d535b119f31c43ea0f38665
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-24 15:46:48 +0530

    fixed searches

--- app/controllers/apiv1/searches_controller.rb
+++ app/controllers/apiv1/searches_controller.rb
@@ -90,16 +90,16 @@ class Apiv1::SearchesController < Apiv1::BaseController
       q.delete(:brand_order_by_phone_eq)
       q.delete(:brand_order_by_online_or_brand_third_party_available_eq)
       @resources[:brands] = Brand.approved.joins(:products).search(q).group("brands.name")
-      @resources[:categories] = nil
-      @resources[:users] = nil
+      @resources[:categories] = {}
+      @resources[:users] = {}
     end
 
     @search = Search.new({
-      :brands => @resources[:brands].nil? ? [] : @resources[:brands].paginate(:per_page => search_result_limit, :page => params[:page]) ,
-      :categories => @resources[:categories].nil? ? [] : @resources[:categories].paginate(:per_page => search_result_limit, :page => params[:page]) ,
-      :locations => @resources[:locations].nil? ? [] : @resources[:locations].paginate(:per_page => search_result_limit, :page => params[:page]) ,
-      :products => @resources[:products].nil? ? [] : @resources[:products].paginate(:per_page => search_result_limit, :page => params[:page]) ,
-      :users => @resources[:users].nil? ? [] : @resources[:users].paginate(:per_page => search_result_limit, :page => params[:page]),
+      :brands => @resources[:brands].nil? ? nil : @resources[:brands].paginate(:per_page => search_result_limit, :page => params[:page]) ,
+      :categories => @resources[:categories].nil? ? nil : @resources[:categories].paginate(:per_page => search_result_limit, :page => params[:page]) ,
+      :locations => @resources[:locations].nil? ? nil : @resources[:locations].paginate(:per_page => search_result_limit, :page => params[:page]) ,
+      :products => @resources[:products].nil? ? nil : @resources[:products].paginate(:per_page => search_result_limit, :page => params[:page]) ,
+      :users => @resources[:users].nil? ? nil : @resources[:users].paginate(:per_page => search_result_limit, :page => params[:page]),
       :pages => {
           :brands => @resources[:brands].nil? ? 0 : (@resources[:brands].length / search_result_limit.to_f).ceil,
           :categories => @resources[:categories].nil? ? 0 : (@resources[:categories].length / search_result_limit.to_f).ceil,
