22b2f5dd0ace1e2963fe45eaa1e5c47e81d59160
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-24 17:30:17 +0530

    fixed location

--- app/models/location.rb
+++ app/models/location.rb
@@ -46,7 +46,9 @@ class Location < ActiveRecord::Base
   api_accessible :Details do |template|
      template.add :products, :template => :product
      template.add :brands, :template => :brand     
-     
+     template.add :id
+     template.add :address
+
   end
 
   api_accessible :location do |template|
