fd8c0aa57fd5aabc60deada650184c1eb4ff44a8
k2b-ramamoorthy <k2b.ramamoorthy@gmail.com>
2014-12-30 15:34:48 +0530

    reduced products on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   	@location = Location.near("#{zip}", 20).collect{|c| c.id}.join(',')
     if @location !=''
        
-  	  @products =  Product.search_products(@location).sortorder().first(12)
+  	  @products =  Product.search_products(@location).sortorder().first(10)
       
       @products_locations = Product.search_products(@location).sortorder().collect{|c| c.location_id}.join(',')
       if @products_locations!=''
@@ -14,7 +14,7 @@ class HomeController < ApplicationController
       end
     else
        
-    @products = Product.search_products().sortorder().first(12)
+    @products = Product.search_products().sortorder().first(10)
     
     end
 
